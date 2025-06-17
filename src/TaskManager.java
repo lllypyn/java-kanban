21a0ff03e41cfe00170d0a60c7b13e81c4db6531
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-06-18 02:24:07 +0300

    добавлены методы, согласно ТЗ

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -38,4 +38,56 @@ public class TaskManager {
         }
         id +=1;
     }
+
+    void printAllTasks() {
+        for (int id : epicTasksList.keySet()) {
+            System.out.println(epicTasksList.get(id));
+        }
+        for (int id : tasksList.keySet()) {
+            System.out.println(tasksList.get(id));
+        }
+    }
+
+    Object searchById(int searchId) {
+        if (epicTasksList.containsKey(searchId)) {
+            return epicTasksList.get(searchId);
+        } else if (tasksList.containsKey(searchId)) {
+            return tasksList.get(searchId);
+        } else {
+            System.out.println("Такой задачи не найдено");
+            return subTasksList.getOrDefault(searchId, null);
+        }
+    }
+
+    void updateTask(int id, String newName, String newDescription) {
+        if (searchById(id).getClass().equals(EpicTask.class) && searchById(id) != null) {
+            EpicTask epicTask = (EpicTask) searchById(id);
+            epicTask.name = newName;
+            epicTask.description = newDescription;
+            epicTasksList.put(id, epicTask);
+        } else if (searchById(id).getClass().equals(Task.class) && searchById(id) != null) {
+            Task task = (Task) searchById(id);
+            task.name = newName;
+            task.description = newDescription;
+            tasksList.put(id, task);
+        } else if (searchById(id).getClass().equals(SubTask.class) && searchById(id) != null) {
+            SubTask subTask = (SubTask) searchById(id);
+            subTask.name = newName;
+            subTask.description = newDescription;
+            subTasksList.put(id, subTask);
+        }
+    }
+
+    void deleteAllSubTasksInEpic(int id) {
+        searchById(id);
+        if (searchById(id).getClass().equals(EpicTask.class) && searchById(id) != null) {
+            EpicTask epicTask = (EpicTask) searchById(id);
+            for (SubTask subTask : epicTask.subIdList) {
+                int delete = subTask.id;
+                subTasksList.remove(delete, subTask);
+            }
+            epicTask.subIdList.clear();
+            epicTasksList.put(id, epicTask);
+        }
+    }
 }
