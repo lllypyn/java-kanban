9b194bc2f11181169fb985638787b35dde7cb22b
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-06-18 02:22:03 +0300

    переопределен toString для подзадачи

--- src/SubTask.java
+++ src/SubTask.java
@@ -2,6 +2,13 @@ public class SubTask extends Task{
 
     int epicId;
 
+    @Override
+    public String toString() {
+
+        return "\n  Подзадача " + id + " " + name + " (" + description + ") " +
+                "относится к глобальной задаче " + epicId +
+                ", статус - " + status;
+    }
 
     SubTask(int id, String name, String description, int epicId) {
         super(id,name,description);
