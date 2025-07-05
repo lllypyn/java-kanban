a1139415faccdead06fbcfb171db3e4c0ec3021d
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-07-05 18:45:58 +0300

    создал утилитарный класс менеджера

---
+++ Test/manager/ManagersTest.java
@@ -0,0 +1,50 @@
+package manager;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import task.EpicTask;
+import task.Status;
+import task.SubTask;
+import task.Task;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagersTest {
+  static InMemoryTaskManager manager = new InMemoryTaskManager();
+
+  @BeforeAll
+  static void create() {
+    manager.addTask(new Task(manager.id, "обычная задача", "пояснение к обычной", Status.NEW));
+    manager.addEpicTask(new EpicTask(manager.id, "Эпик задача", "пояснение к эпику"));
+    manager.addSubTask(new SubTask(manager.id, "Субтаск", "пояснение к субтаску", 1, Status.DONE));
+    manager.addSubTask(new SubTask(manager.id, "субтаск 2", "пояснение к субтаску 2", 1, Status.IN_PROGRESS));
+    manager.addTask(new Task(manager.id, "обычная задача2", "пояснение к обычной2", Status.DONE));
+  }
+
+  @Test
+  void checkListHistoryIsEmpty() {
+    assertNull(manager.inMemoryHistoryManager.getHistory(), "размер списка историй не совпадает с ожидаемым 0");
+  }
+
+  @Test
+  void checkForHistoryChanges() {
+    manager.searchById(4);
+    manager.searchById(0);
+    assertEquals(2, manager.inMemoryHistoryManager.getHistory().size(), "История просмотров задач не изменяется");
+  }
+
+  @Test
+  void updatingHistoryWhenAdding11Tasks() {
+    manager.searchById(0);
+    for (int i = 0; i < 9; i++) {
+      manager.searchById(1);
+    }
+    manager.searchById(2);
+    assertEquals(manager.inMemoryHistoryManager.getHistory().getLast(), manager.searchById(2));
+    assertEquals(manager.inMemoryHistoryManager.getHistory().getFirst(), manager.searchById(1));
+  }
+  @Test
+  void checkHistoryWhenSearching() {
+
+  }
+}
