353de7e4c03d91b3c18aee33732039491c3a30e2
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-07-05 18:39:12 +0300

    создал класс истории

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import manager.TaskManager;
+import manager.InMemoryTaskManager;
 import task.EpicTask;
 import task.Status;
 import task.SubTask;
@@ -7,47 +7,47 @@ import task.Task;
 public class Main{
 
     public static void main(String[] args) {
-        TaskManager taskManager = new TaskManager();
+        InMemoryTaskManager manager = new InMemoryTaskManager();
 
         //## Tests
         //# добавление
 
         for (int i = 0; i < 5; i++){
-            System.out.println("добавляем глобальную задачу " + taskManager.id);
-            taskManager.addEpicTask(new EpicTask(taskManager.id, "Гл.задача", "Гл.пояснение"));
+            System.out.println("добавляем глобальную задачу " + manager.id);
+            manager.addEpicTask(new EpicTask(manager.id, "Гл.задача", "Гл.пояснение"));
             for (int j = 0; j < 9; j++){
-                taskManager.addSubTask(new SubTask(taskManager.id, "подзадача", "подз.пояснение",
+                manager.addSubTask(new SubTask(manager.id, "подзадача", "подз.пояснение",
                         (i * 10), Status.DONE));
             }
         }
         for (int i = 0; i < 5; i++){
-            System.out.println("добавляем обычную задачу" + taskManager.id);
-            taskManager.addTask(new Task(taskManager.id, "об.задача", "об.пояснение", Status.NEW));
+            System.out.println("добавляем обычную задачу" + manager.id);
+            manager.addTask(new Task(manager.id, "об.задача", "об.пояснение", Status.NEW));
         }
 
-        taskManager.printAllTasks();
+        manager.printAllTasks();
         //# обновление
-        taskManager.updateTask(new Task(2, "купить продукты", "сходить в магазин", Status.NEW));
-        taskManager.updateSubTask(new SubTask(9, "сесть", "встать", 0, Status.NEW));
-        taskManager.updateEpicTask(new EpicTask(3, "Отпраздновать день рождения", "Устроить все как надо"));
-        System.out.println(taskManager.searchById(2));
+        manager.updateTask(new Task(2, "купить продукты", "сходить в магазин", Status.NEW));
+        manager.updateSubTask(new SubTask(9, "сесть", "встать", 0, Status.NEW));
+        manager.updateEpicTask(new EpicTask(3, "Отпраздновать день рождения", "Устроить все как надо"));
+        System.out.println(manager.searchById(2));
         //# удаление
-        taskManager.deleteTask(4);
-        taskManager.deleteSubTask(14);
-        taskManager.deleteEpicTask(10);
-        taskManager.deleteAllSubTasks();
-        taskManager.addTask(new Task(taskManager.id, "Убраться", "навести порядок дома", Status.DONE));
-        taskManager.addEpicTask(new EpicTask(taskManager.id, "Привести машину в порядок", "Приготовить ее к поездке"));
-        taskManager.addSubTask(new SubTask(taskManager.id, "приготовить еду", "Сделать салаты", 0, Status.DONE));
-        taskManager.addSubTask(new SubTask(taskManager.id, "разложить стол", "он в кладовке", 3, Status.IN_PROGRESS));
-        taskManager.addSubTask(new SubTask(taskManager.id, "накрыть на стол", "как скатерть, так и еду", 3, Status.DONE));
-        taskManager.addSubTask(new SubTask(taskManager.id, "позвать гостей", "можно и через мессенджеры", 3, Status.DONE));
-        taskManager.deleteAllTasks();
-        System.out.println(taskManager.searchById(5));
-        taskManager.deleteAllSubTasksInEpic(0);
-        taskManager.printAllTasks();
-        taskManager.deleteAllEpicTasks();
-        taskManager.deleteAll();
-        taskManager.printAllTasks();
+        manager.deleteTask(4);
+        manager.deleteSubTask(14);
+        manager.deleteEpicTask(10);
+        manager.deleteAllSubTasks();
+        manager.addTask(new Task(manager.id, "Убраться", "навести порядок дома", Status.DONE));
+        manager.addEpicTask(new EpicTask(manager.id, "Привести машину в порядок", "Приготовить ее к поездке"));
+        manager.addSubTask(new SubTask(manager.id, "приготовить еду", "Сделать салаты", 0, Status.DONE));
+        manager.addSubTask(new SubTask(manager.id, "разложить стол", "он в кладовке", 3, Status.IN_PROGRESS));
+        manager.addSubTask(new SubTask(manager.id, "накрыть на стол", "как скатерть, так и еду", 3, Status.DONE));
+        manager.addSubTask(new SubTask(manager.id, "позвать гостей", "можно и через мессенджеры", 3, Status.DONE));
+        manager.deleteAllTasks();
+        System.out.println(manager.searchById(5));
+        manager.deleteAllSubTasksInEpic(0);
+        manager.printAllTasks();
+        manager.deleteAllEpicTasks();
+        manager.deleteAll();
+        manager.printAllTasks();
     }
 }
---
+++ src/manager/InMemoryHistoryManager.java
@@ -0,0 +1,39 @@
+package manager;
+
+import task.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+
+    public static List<Task> history = new ArrayList<>(10);
+    InMemoryHistoryManager(){
+
+    }
+
+
+    @Override
+    public List<Task> getHistory() {
+        if (history.isEmpty()){
+            return null;
+        } else {
+            return history;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return history.toString();
+    }
+
+    @Override
+    public void add (Task task){
+        if (history.size()==10 && task != null){
+            history.removeFirst();
+        }
+        history.add(task);
+
+    }
+}
