5cecc20c2f593645673ba02c8f3508dd69e9fef2
Alexandr Lagutin <lagutiiin@yandex.ru>
2025-08-03 15:41:33 +0300

    обновлены методы работы со списком истории

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -39,8 +39,62 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-        if (history.size() == 10 && task != null) {
-            history.removeFirst();
+        if(historyMap.containsKey(task.getId())){
+             removeNode(historyMap.get(task.getId()));
+             historyMap.remove(task.getId());
+        }
+        linkLast(task);
+    }
+    @Override
+    public void clear() {
+        Node<Task> current = head;
+        while (current != null) {
+            Node<Task> next = current.next;
+            current.data = null;
+            current.prev = null;
+            current.next = null;
+            current = next;
+        }
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    List<Task> getTasks() {
+        List<Task> list = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null){
+            list.add(current.data);
+            current = current.next;
+        }
+        return list;
+    }
+
+    void removeNode(Node<Task> node){
+        Node<Task> prev = node.prev;
+        Node<Task> next = node.next;
+        if (prev == null){
+            head = next;
+        } else {
+            prev.next = next;
+        }
+        if (next == null){
+            tail = prev;
+        } else {
+            next.prev = prev;
+        }
+        size--;
+    }
+
+    void linkLast(Task task) {
+
+        Node<Task> node = new Node<>(task);
+        if (tail == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev =tail;
+            tail = node;
         }
         history.add(task);
     }
