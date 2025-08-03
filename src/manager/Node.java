36e107d8b5eb7b6cb8c8fd3db0e0fc47672fda42
lllypyn <lagutiiin@yandex.ru>
2025-08-03 16:13:55 +0300

    Update Node.java

--- src/manager/Node.java
+++ src/manager/Node.java
@@ -1,11 +1,11 @@
 package manager;
 
-public class Node<Task> {
+public class Node<T> {
     public Task data;
-    public Node<Task> next;
-    public Node<Task> prev;
+    public Node<T> next;
+    public Node<T> prev;
 
-    public Node(Task data) {
+    public Node(T data) {
         this.data = data;
         this.next = null;
         this.prev = null;
